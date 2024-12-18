3ff26dd35088f68d3854c4d998b6d2aa79cd8465
William Au <williamau16@gmail.com>
2024-12-19 05:00:30 +0800

    webdriver change: CHROME -> EDGE
    cleanups

--- browser_automation.py
+++ browser_automation.py
@@ -1,10 +1,9 @@
 # %%
 # pip install selenium-wire
-# pip install blinker==1.7.0
 
 from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.edge.options import Options
+
 import time
 import os
 import shutil
@@ -12,55 +11,60 @@ import shutil
 # Download files from the URL
 def download_url(url):
     print('Download URL: ', url)
+
     # Create folder 'tempdata' to store RVC csv files
     script_dir = os.path.join(os.getcwd(),'tempdata')
     download_dir = script_dir
-
-    capabilities = DesiredCapabilities().CHROME
-
-    # Remove unecessary pop ups and error logs
-    chrome_options = Options()
-    chrome_options.add_argument('--headless=new')
-    chrome_options.add_argument('--log-level=3')
+    
+    print('download_dir: ', download_dir)
+    #capabilities = DesiredCapabilities().EDGE
 
     prefs = {'download.default_directory':download_dir,
              'download.prompt_for_download':False,
-             'download.directory_upgrade':True}
-    
-    chrome_options.add_experimental_option('prefs', prefs)
-    capabilities.update(chrome_options.to_capabilities())
+             'download.directory_upgrade':True,}
 
-    driver = webdriver.Chrome(options=chrome_options)
-    x= True
+    # Remove unecessary pop ups and error logs
+    edge_options = Options()
+    edge_options.add_experimental_option('prefs', prefs)
+    edge_options.add_argument('headless')
+    edge_options.add_argument('log-level=3')
+    
+    driver = webdriver.Edge(options=edge_options)
+    retry = True
 
     safety_counter = 0
     code = '0'
 
-    while x:
+    while retry:
         if(safety_counter>10):
             print("Retry limit reached, cancelling download...")
             return code
-
+        
         driver.get(url)
         print('...')
 
-        # Access requests via the `requests` attribute
+        # Access requests via the 'requests' attribute
         for request in driver.requests:
             if request.response and request.url == url:
+                
                 # Repeat process until successful status code 200
                 code = request.response.status_code
+                
                 if request.response.status_code == 200:
-                    x=False
+                    retry=False # Stop retrying once successful
                     time.sleep(1)
+                    
                     print(
                         'Response Code: ', 
                         request.response.status_code
                     )
+                    
                     print('Download Successful ------------------\n')
-                    x=False
-        if not x:
-            driver.close()
-            break
+            
+            if not retry:
+                driver.close()
+                break
+        
         safety_counter = safety_counter+1
 
 # Delete tempfolder
